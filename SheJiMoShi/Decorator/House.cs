690484efa160d32acf88e54e5d1dbaa95e983a36
马鑫 <anyangmaxin@163.com>
2015-11-07 18:05:45 +0800

    装饰模式半成品

---
+++ SheJiMoShi/Decorator/House.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    /// <summary>
+    /// 抽象对象接口
+    /// </summary>
+   abstract class House
+   {
+       public abstract void Show();
+   }
+}
---
+++ SheJiMoShi/Decorator/PingFang.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+   abstract class PingFang:House
+    {
+        public override void Show()
+        {
+           Console.WriteLine("这是平房");
+        }
+    }
+}
